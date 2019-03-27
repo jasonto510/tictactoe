1bbe0123e30840cc04ceec725521c34c754715af
Mac HD <machd@Macs-MacBook-Pro.local>
2019-03-27 14:04:29 -0700

    created test case for draw and logic for the game being played

--- tictactoe.js
+++ tictactoe.js
@@ -64,3 +64,28 @@ let rightDiagonal = function(piece){
   }
   return false
 }
+
+let draw = function(count) {
+  if (count === 9){
+    return true;
+  }
+  return false;
+}
+
+let playerWon = function(piece, row, column) {
+  if (rightDiagonal(piece) || leftDiagonal(piece) || horizontalWin(piece, row) || verticalWin(piece, column)) {
+    return true;
+  }
+  return false;
+}
+
+let turnPlayed = true;
+
+let playGame = function() {
+  while (!playerWon || !draw) {
+    //Get each player to toggle a move
+    //If turn played is true then toggle x 
+    //Else toggle y 
+   //Switch the state of turnplayed after every move
+  }
+}
