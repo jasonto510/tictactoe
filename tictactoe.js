ba19097cb3a60683ae04ddd666400d1d91e4910b
Mac HD <machd@Macs-MacBook-Pro.local>
2019-03-27 14:24:29 -0700

    refactoring some game logic

--- tictactoe.js
+++ tictactoe.js
@@ -6,7 +6,7 @@ let board = [['', '', ''],
 let playerOptions = ['x', 'o'];
 
 let togglePiece = function(piece, row, column) {
-  if (board[row][column] !== '') {
+  if (board[row][column] === '') {
     board[row][column] = piece;
   } else {
     alert('this is an invalid move');
@@ -54,8 +54,8 @@ let leftDiagonal = function(piece) {
 
 let rightDiagonal = function(piece){
   let count = 0;
-  for (var i = board.length; i >= 0; i++){
-    if (board[board.length - i][i]){
+  for (var i = board.length; i >= 0; i--){
+    if (board[board.length - i][i - 1] === piece){
       count ++;
     }  
   }
@@ -64,6 +64,8 @@ let rightDiagonal = function(piece){
   }
   return false
 }
+
+
 
 let draw = function(count) {
   if (count === 9){
@@ -84,15 +86,18 @@ let playGame = function() {
   let turnPlayed = true;
   let count = 0;
   let currentPiece = 'x'
-  while (!playerWon || !draw) {
-    if (togglePiece(currentPiece, row, column)){
+  let gameOver = false;
+  let result = ''
+  while (!gameOver) {
+    if (togglePiece(currentPiece, row, column)) {
       count ++;
     }
-    //After every move test if a player has won or if it's a draw 
-    playerWon(piece, row, column);
-    draw(count);
-
-    //Switches between users
+    if (playerWon(piece, row, column)) {
+      gameOver = true;
+      result = `Congratulations to player ${currentPiece}`
+    } else if (draw(count)){
+      result = 'This game ended in a draw'
+    }
     if (turnPlayed){
       turnPlayed = !turnPlayed;
       currentPiece = 'x'
@@ -100,9 +105,6 @@ let playGame = function() {
       turnPlayed = !turnPlayed;
       currentPiece = 'o'      
     }
-    //Get each player to toggle a move
-    //If turn played is true then toggle x 
-    //Else toggle y 
-   //Switch the state of turnplayed after every move
   }
+  return result;
 }
